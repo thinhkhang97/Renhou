a086b2662600b8942e82e0962655749360785e9d
Khang Thinh Nguyen <khangthinhnguyen@FVFY81R2HV2H.local>
2019-04-03 22:07:57 +0700

    Merge from dev

--- App.js
+++ App.js
@@ -7,14 +7,6 @@
  */
 
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import CalculateBill from './src/components/CalculateBill';
-import { RoomDetail } from "./src/screen";
-import Statistic from './src/components/Statistic';
-
-=======
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import CalculateBill from './src/components/CalculateBill';
@@ -43,23 +35,13 @@ const AppNavigator = createStackNavigator({
 const AppContainer = createAppContainer(AppNavigator);
 
 
->>>>>>> develop
 class App extends Component {
   
   render() {
     const {navigation} = this.props;
     return (
       <View style={styles.container}>
-<<<<<<< HEAD
-        <Text>hello world</Text>
-        <TouchableOpacity onPress={()=>{
-          navigation.navigate("roomDetail");
-        }}>
-        <Text>RoomDetail</Text>
-        </TouchableOpacity>
-=======
         <AppContainer />
->>>>>>> develop
       </View>
     );
   }
