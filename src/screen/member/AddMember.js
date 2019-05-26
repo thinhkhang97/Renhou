ec90e09757ada1045d21a639118237905b946801
Khang Thinh Nguyen <khangthinhnguyen@FVFY81R2HV2H.local>
2019-05-26 15:48:33 +0700

    Modify UI of add member

--- src/screen/member/AddMember.js
+++ src/screen/member/AddMember.js
@@ -1,107 +1,164 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { StyleSheet, ScrollView, View, TextInput, Alert, Text } from 'react-native';
-import { Input } from '../../components/baseComponent';
-import Global from '../../Global';
-import { MainButton } from '../../components/baseComponent';
-import { addMember } from '../../store/actions/memberAction';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  TextInput,
+  Alert,
+  Text,
+  KeyboardAvoidingView
+} from "react-native";
+import { Input } from "../../components/baseComponent";
+import Global from "../../Global";
+import { MainButton } from "../../components/baseComponent";
+import { addMember } from "../../store/actions/memberAction";
 import { memberServices } from "../../services";
 class AddMember extends Component {
-    static navigationOptions = () => {
-        return {
-            title: 'Thêm thành viên',
-            headerStyle: {
-                backgroundColor: 'white',
-            },
-            headerTitleStyle: {
-                color: Global.COLOR.NAVIGATION,
-            },
-            headerTintColor: Global.COLOR.NAVIGATION,
-        };
+  static navigationOptions = () => {
+    return {
+      title: "Thêm thành viên",
+      headerStyle: {
+        backgroundColor: "white"
+      },
+      headerTitleStyle: {
+        color: Global.COLOR.NAVIGATION
+      },
+      headerTintColor: Global.COLOR.NAVIGATION
     };
-    state = {
-        name: '',
-        identifier: '',
-        phone: '',
-        adding: false,
+  };
+  state = {
+    name: "",
+    identifier: "",
+    phone: "",
+    dob: "",
+    email: "",
+    adding: false
+  };
+
+  onPressAddRoom = () => {
+    const { name, identifier, phone } = this.state;
+    this.setState({ adding: true });
+    const { navigation, userID, token, addRoom } = this.props;
+    const roomID = navigation.getParam("roomID");
+    const data = {
+      userId: userID,
+      name
     };
+    // roomServices.addRoom(data, token).then(res => {
+    //     addRoom(res.data.data.room);
+    //     navigation.goBack();
+    // }).catch(error => {
+    //     Alert.alert('Lỗi', error.response.data.data.message);
+    //     navigation.goBack();
+    // });
+  };
 
-    onPressAddRoom = () => {
-        const { name, identifier, phone } = this.state;
-        this.setState({ adding: true });
-        const { navigation, userID, token, addRoom } = this.props;
-        const roomID = navigation.getParam('roomID');
-        const data = {
-            userId: userID,
-            name,
-        }
-        // roomServices.addRoom(data, token).then(res => {
-        //     addRoom(res.data.data.room);
-        //     navigation.goBack();
-        // }).catch(error => {
-        //     Alert.alert('Lỗi', error.response.data.data.message);
-        //     navigation.goBack();
-        // });
-    }
+  styleDOB(dob) {
+    return dob
+      .toString()
+      .replace(/\B([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/g, "-");
+  }
 
-    render() {
-        return (
-            <ScrollView style={styles.container}>
-                <View style={styles.row}>
-                    <Input value={this.state.name} placeholder={'Họ và tên'} onChangeText={text => this.setState({ name: text })} />
-                </View>
-                <View style={styles.row}>
-                    <Input value={this.state.phone} keyboardType={'phone-pad'} placeholder={'Số điện thoại'} onChangeText={text => this.setState({ phone: text })} />
-                </View>
-                <View style={styles.row}>
-                    <Input value={this.state.identifier} keyboardType={'number-pad'} placeholder={'CMND'} onChangeText={text => this.setState({ identifier: text })} />
-                </View>
-                <MainButton title='Thêm' disabled={this.state.adding} onPress={this.onPressAddRoom} />
-            </ScrollView >
-        );
-    }
+  render() {
+    return (
+      <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+        <ScrollView style={styles.container}>
+          <View style={styles.row}>
+            <Input
+              value={this.state.name}
+              placeholder={"Họ và tên"}
+              onChangeText={text => this.setState({ name: text })}
+            />
+          </View>
+          <View style={styles.row}>
+            <Input
+              value={this.state.phone}
+              keyboardType={"phone-pad"}
+              placeholder={"Số điện thoại"}
+              onChangeText={text => this.setState({ phone: text })}
+            />
+          </View>
+          <View style={styles.row}>
+            <Input
+              value={this.state.identifier}
+              keyboardType={"number-pad"}
+              placeholder={"CMND"}
+              onChangeText={text => this.setState({ identifier: text })}
+            />
+          </View>
+          <View style={styles.row}>
+            <Input
+              value={this.state.identifier}
+              keyboardType={"number-pad"}
+              placeholder={"Ngày tháng năm sinh"}
+              onChangeText={text => {
+                this.setState({
+                  dob: text
+                });
+              }}
+            />
+          </View>
+          <View style={styles.row}>
+            <Input
+              value={this.state.identifier}
+              keyboardType={"email-address"}
+              placeholder={"Email"}
+              onChangeText={text => this.setState({ email: text })}
+            />
+          </View>
+          <MainButton
+            title="Thêm"
+            disabled={this.state.adding}
+            onPress={this.onPressAddRoom}
+          />
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: Global.COLOR.BACKGROUND,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-    },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderBottomWidth: 0.5,
-        borderColor: Global.COLOR.GRAY,
-        paddingVertical: 10,
-    },
-    text: {
-        flex: 1,
-        paddingVertical: 5,
-        fontSize: 16,
-        textAlign: 'left',
-        borderRadius: 12,
-    },
-    data: {
-        flex: 2,
-        fontSize: 16,
-        textAlign: 'right',
-        color: Global.COLOR.GRAY,
-    },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: Global.COLOR.BACKGROUND,
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 0.5,
+    borderColor: Global.COLOR.GRAY,
+    paddingVertical: 10
+  },
+  text: {
+    flex: 1,
+    paddingVertical: 5,
+    fontSize: 16,
+    textAlign: "left",
+    borderRadius: 12
+  },
+  data: {
+    flex: 2,
+    fontSize: 16,
+    textAlign: "right",
+    color: Global.COLOR.GRAY
+  }
 });
 
-const mapStateToProps = (state) => ({
-    token: state.authenticationReducer.accessToken,
-    userID: state.authenticationReducer.userID,
-})
+const mapStateToProps = state => ({
+  token: state.authenticationReducer.accessToken,
+  userID: state.authenticationReducer.userID
+});
 
-const mapDispatchToProps = (dispatch) => ({
-    addRoom: (data) => dispatch(addRoom(data)),
-})
+const mapDispatchToProps = dispatch => ({
+  addRoom: data => dispatch(addRoom(data))
+});
 
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddMember);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddMember);
